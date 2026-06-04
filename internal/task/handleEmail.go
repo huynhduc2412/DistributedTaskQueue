caea0435fc06ae3217d548eba022af0456ee081c
huynhduc <huynhducmapu2002@gmail.com>
2026-06-05 01:54:39 +0700

    random retry task

--- internal/task/handleEmail.go
+++ internal/task/handleEmail.go
@@ -2,6 +2,7 @@ package task
 
 import (
 	"context"
+	"errors"
 	"fmt"
 	"math/rand/v2"
 	"time"
@@ -10,6 +11,10 @@ import (
 func HandleEmail(ctx context.Context, payload map[string]interface{}) error {
 	//random 2->6s exucute
 	time.Sleep(time.Duration(rand.IntN(5) + 2) * time.Second)
+	r := rand.IntN(2)
+	if r == 0 {
+		return errors.New("random wrong")
+	}
 	fmt.Printf("Send successfully to : %v\n", payload["to"])
 	return nil
 }
